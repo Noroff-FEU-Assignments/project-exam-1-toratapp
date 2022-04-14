d3995219c49147489238b65495d984e107b9313b
toratapp <73744488+toratapp@users.noreply.github.com>
2022-04-14 23:06:02 +0200

    Making homepage
    Writing the HTML and some CSS and js

---
+++ js/components/footer.js
@@ -0,0 +1,17 @@
+const footer = document.querySelector("footer");
+
+footer.innerHTML += `<div class="short-menu">
+                        <h4>Short menu</h4>
+                        <a href="blog.html">Blog</a>
+                        <a href="#">Sewing glossary</a>
+                        <a href="#">Projects gallery</a>
+                    </div>
+                    <div class="social-media">
+                        <div class="social-media__icons">
+                        </div>
+                        <p>Follow for more</p>
+                        <p>&#64;onthebiasblog</p>
+                    </div>
+                    <div class="copyright">
+                        <p>Copyright &copy; 2022 &hyphen; On the bias blog</p>
+                    </div>`;
---
+++ js/home.js
@@ -0,0 +1,15 @@
+const url = "https://teidsvag.com/onthebias-cms/wp-json/wp/v2/posts?_embed";
+const postsMobileContainer = document.querySelector(".latest-posts__mobile");
+
+async function getPosts() {
+    try {
+        const response = await fetch(url);
+        const results = await response.json();
+        console.log(results);
+    } catch(error) {
+        console.warn(error);
+        return postsMobileContainer.innerHTML = errorMessage("An error occured while fetching the posts")
+    }
+}
+
+getPosts();
